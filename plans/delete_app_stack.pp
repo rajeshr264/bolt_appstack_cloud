a180e40152a3f27973482a129c2909004242271a
Rajesh Radhakrishnan <>
2024-01-25 13:54:47 -0800

    added deletion of a stack

--- plans/delete_app_stack.pp
+++ plans/delete_app_stack.pp
@@ -1,8 +1,13 @@
 plan bolt_appstack_cloud::delete_app_stack(
-  TargetSpec $targets
+  TargetSpec $targets, 
+  String $generated_inventory_filename,
 ) {
+  # parse the yaml file, get the names of the nodes
+  $generated_vms = loadyaml($generated_inventory_filename)
+  #out::message("Inventory: ${generated_vms}")
 
-    
-
-
+  $generated_vms['targets'].each |$app| {
+    #out::message("Deleting ${app}")
+    run_task("bolt_appstack_cloud::delete_vm", $targets, 'vm_name' => $app['name'])
+  }
 }
--- plans/pe_install.pp
+++ plans/pe_install.pp
@@ -14,13 +14,14 @@ plan bolt_appstack_cloud::pe_install(
   $target_hash = {'primary_host' => $target}
   $args_hash = $peadm_args_hash + $target_hash
 
+  # Wierd Situation: for freshly created VMs, bolt/ssh is not able to connect to the VM. Hence have to run it twice.
   $result_or_error = catch_errors(['bolt/run-failure']) || {
     run_plan($peadm_install_plan,$args_hash)
   }
   if $result_or_error =~ Error {
     $msg = $result_or_error.details['result_set'][0].error().message()
-    if $msg =~ 'No route to host' {
-      run_plan($peadm_install_plan,$args_hash)
+    if $msg =~ 'No route to host' { # this is the SSH error message when connecting first time.
+      run_plan($peadm_install_plan,$args_hash) # run peadm::install again
     } else {
       fail_plan($result_or_error)
     }
